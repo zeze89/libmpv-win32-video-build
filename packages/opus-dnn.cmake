d96f62b96d2b127353abb424da868e8587ed61d8
zeze89 <129963271+zeze89@users.noreply.github.com>
2026-08-23 16:36:50 +0300

    opus-dnn: add model download package (from upstream)

---
+++ packages/opus-dnn.cmake
@@ -0,0 +1,14 @@
+# Upstream'den birebir (2026-08-23): opus main'in DNN model dosyalari
+# depoda degil, ayri tarball'dan geliyor; olmadan meson 'fargan_data.h
+# does not exist' ile dusuyor (32642631587).
+ExternalProject_Add(opus-dnn
+    URL https://media.xiph.org/opus/models/opus_data-8a07d57c4fce6fb30f23b3e0d264004e04f1d7b421f5392ef61543d021a439af.tar.gz
+    URL_HASH SHA256=8a07d57c4fce6fb30f23b3e0d264004e04f1d7b421f5392ef61543d021a439af
+    DOWNLOAD_DIR ${SOURCE_LOCATION}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+cleanup(opus-dnn install)
