59dc7ad17e5fd8dd052632ef1af9c63c093953f6
zeze89 <zz89842015@gmail.com>
2026-08-23 18:09:56 +0300

    angle-headers: pin like upstream (mpv 2023 needs D3D9 constants, measured)

--- packages/angle-headers.cmake
+++ packages/angle-headers.cmake
@@ -3,6 +3,11 @@ ExternalProject_Add(angle-headers
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_REMOTE_NAME origin
     GIT_TAG main
+    # Upstream ile ayni pin (2026-08-23): ANGLE guncel basliklari D3D9
+    # sabitlerini kaldirdi, mpv 2023 ise bekliyor (context_angle.c:336
+    # 'EGL_PLATFORM_ANGLE_TYPE_D3D9_ANGLE' tanimsiz, 32647187371). mpv'nin
+    # kullandigi 10 ANGLE sabitinin hepsinin bu pinde var oldugu OLCULDU.
+    GIT_RESET 0cb8023c01f92b29f3738ea7472d06f8f059ed84
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
     GIT_CLONE_POST_COMMAND "sparse-checkout set include/EGL include/KHR"
     GIT_SUBMODULES ""
